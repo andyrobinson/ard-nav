8a70aa49d4116f0910f4215967be3898658d9e0e
Andy Robinson <andy.robinson8@gmail.com>
2019-11-24 21:51:38 +0000

    GPS All working now :-)

--- gps/gps.ino
+++ gps/gps.ino
@@ -16,6 +16,20 @@
 
 #include <Adafruit_GPS.h>
 
+// Serial2 pin and pad definitions (in Arduino files Variant.h & Variant.cpp)
+#define PIN_SERIAL2_RX       (34ul)               // Pin description number for PIO_SERCOM on D12
+#define PIN_SERIAL2_TX       (36ul)               // Pin description number for PIO_SERCOM on D10
+#define PAD_SERIAL2_TX       (UART_TX_PAD_2)      // SERCOM pad 2
+#define PAD_SERIAL2_RX       (SERCOM_RX_PAD_3)    // SERCOM pad 3
+
+// Instantiate the Serial2 class
+Uart Serial2(&sercom1, PIN_SERIAL2_RX, PIN_SERIAL2_TX, PAD_SERIAL2_RX, PAD_SERIAL2_TX);
+
+void SERCOM1_Handler()    // Interrupt handler for SERCOM1
+{
+  Serial2.IrqHandler();
+}
+
 // what's the name of the hardware serial port?
 #define GPSSerial Serial2
 
@@ -28,14 +42,13 @@ Adafruit_GPS GPS(&GPSSerial);
 
 uint32_t timer = millis();
 
-
 void setup()
 {
   while (!Serial);  // uncomment to have the sketch wait until Serial is ready
 
   // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
   // also spit it out
-  Serial.begin(115200);
+  Serial.begin(19200);
   Serial.println("Adafruit GPS library basic test!");
 
   // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
@@ -72,7 +85,7 @@ void loop() // run over and over again
     // a tricky thing here is if we print the NMEA sentence, or data
     // we end up not listening and catching other sentences!
     // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    //Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
     if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
       return; // we can fail to parse a sentence in which case we should just wait for another
   }
@@ -80,7 +93,7 @@ void loop() // run over and over again
   if (timer > millis()) timer = millis();
 
   // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
+  if (millis() - timer > 10000) {
     timer = millis(); // reset the timer
     Serial.print("\nTime: ");
     if (GPS.hour < 10) { Serial.print('0'); }
@@ -103,9 +116,10 @@ void loop() // run over and over again
     Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
     if (GPS.fix) {
       Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(GPS.latitude_fixed, 4); Serial.print(GPS.lat);
       Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print(GPS.longitude_fixed, 4); Serial.println(GPS.lon);
+      Serial.print("Precision: ");Serial.println(GPS.PDOP);
       Serial.print("Speed (knots): "); Serial.println(GPS.speed);
       Serial.print("Angle: "); Serial.println(GPS.angle);
       Serial.print("Altitude: "); Serial.println(GPS.altitude);
