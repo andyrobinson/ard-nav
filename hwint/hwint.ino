af5e425bb78354dfb3e8eb4d77c43fa479f4b464
Andy Robinson <andy.robinson8@gmail.com>
2020-01-19 19:24:00 +0000

    Everything apart from GPS tested ...

--- hwint/hwint.ino
+++ hwint/hwint.ino
@@ -1,12 +1,22 @@
 #include <Compass.h>
 #include <WindSensor.h>
 #include <Position.h>
+#include <Rudder.h>
+#include <Sail.h>
 // pick between loggers here
 #include <DisplayLogger.h>
 //#include <SerialLogger.h>
 
+#define SAIL_SERVO_PIN 6
+#define RUDDER_SERVO_PIN 5
+
 using namespace Angle;
 using namespace Position;
+
+Servo rudder_servo;
+Rudder rudder(&rudder_servo);
+Servo sail_servo;
+Rudder sail(&sail_servo);
 
 WindSensor windsensor;
 Compass compass;
@@ -17,12 +27,27 @@ void setup() {
   logger.begin();
   windsensor.begin();
   compass.begin();
+  rudder_servo.attach(RUDDER_SERVO_PIN);
+  sail_servo.attach(SAIL_SERVO_PIN);
+}
+
+void move_rudder() {
+  static angle rudder_position = 0;
+  static short rudder_increment = 5;
+
+  rudder_position = rudder_position + rudder_increment;
+  if (abs(rudder_position) >= RUDDER_MAX_DISPLACEMENT) {
+    rudder_increment = -rudder_increment;
+  }
+  rudder.set_position(rudder_position);
 }
 
 void loop() {
   angle wind = windsensor.relative();
   uangle bearing = compass.bearing();
-  logger.info(&current_position, wind, bearing, "Testing wind and bearing and a bit");
+  move_rudder();
+  sail.set_position(wind);
+  logger.info(&current_position, wind, bearing, "All items");
 
   delay(1000);
 }
