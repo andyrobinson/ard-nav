c26b43612cd455194688f2e76a4a702042c00717
Andy Robinson <andy.robinson8@gmail.com>
2020-03-15 12:32:14 +0100

    tidying up sail.ino

--- sail/sail.ino
+++ sail/sail.ino
@@ -14,34 +14,16 @@ Servo sail_servo;
 Sail sail(&sail_servo);
 
 void setup() {
-  Serial.begin(19200);
-  Serial.println(F("Initialising"));
-
   windsensor.begin();
   sail_servo.attach(SAIL_SERVO_PIN);
 }
 
-void log(int w, int s) {
-  Serial.print("wind: ");
-  Serial.print(w);
-  Serial.print(" sail angle:");
-  Serial.println(s);
-}
-
 void loop() {
-  int printcount = 0;
   angle wind_angle = 0;
   angle servo_angle = 0;
 
-  printcount = (printcount + 1) % 100;
-
-  if (printcount == 0) {
-    log(wind_angle, servo_angle);
-  }
-
   wind_angle = windsensor.relative();
   sail.set_position(wind_angle);
-  servo_angle = sail_servo.read();
 
   delay(10);
 }
