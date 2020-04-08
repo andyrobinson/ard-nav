33f0ff22325decfa8fe34d9a9e2603199c03c1fd
Andy Robinson <andy.robinson01@bbc.co.uk>
2020-04-08 21:40:39 +0100

    SelfTest now compiles

--- lib/SelfTest/SelfTest.cpp
+++ lib/SelfTest/SelfTest.cpp
@@ -2,20 +2,70 @@
 
 SelfTest::SelfTest() {}
 
-SelfTest(Gps *gpsp, WindSensor *windsensorp, Compass *compassp, Sail *sailp, Rudder *rudderp);
-  gps(gpsp), windsensor(windsensorp), compass(compassp), sail(sailp), rudder(rudderp) {}
+SelfTest::SelfTest(Gps *gpsp, WindSensor *windsensorp, Compass *compassp, Sail *sailp, Rudder *rudderp, Timer *timerp):
+  gps(gpsp), windsensor(windsensorp), compass(compassp), sail(sailp), rudder(rudderp), timer(timerp) {}
 
+void SelfTest::next() {
+  rudder->set_position(NEXT_TEST);
+  timer->wait(SERVO_WAIT);
+  rudder->set_position(IN_TEST);
+  timer->wait(SERVO_WAIT);
+}
+
+void SelfTest::read_gps(gpsResult *result) {
+  int gps_wait = 2000;
+  boolean fix = false;
+  while (gps_wait < 30000 && !fix) {
+    *result = gps->data(gps_wait);
+    fix = result->fix < FIX_GPS;
+    if (!fix) {
+      gps_wait = gps_wait + 5000;
+    }
+  }
+}
+
+// we don't return a result because we just carry on.  This is only for a human
 void SelfTest::test() {
-  // rudder full right, then
-  // rudder left, in test
+  boolean ok = true;
+  gpsResult gpsData;
 
-    // WindSensor
-    // Compass
-    // Sail
-    // Gps - how do we set this going and carry on?
-    
+  // rudder test
+  rudder->set_position(-45);
+  timer->wait(SERVO_WAIT);
+  rudder->set_position(45);
+  timer->wait(SERVO_WAIT);
+
+  // WindSensor
+  next();
+  angle result = windsensor->relative();
+  ok = (ok && result != NO_WIND_VALUE);
+
+  // Compass
+  next();
+  uangle bearing = compass->bearing();
+  ok = (ok && (bearing >=0 && bearing <=360));
+
+  // Sail
+  next();
+  sail->set_position(-120);
+  timer->wait(1500);
+  sail->set_position(120);
+  timer->wait(1500);
+  sail->set_position(0);
+  // cannot update ok
+
+  // Gps
+  next();
+  read_gps(&gpsData);
+  ok = (ok && gpsData.fix >= FIX_GPS);
+
   // rudder right, tests failed
+  if (ok) {
+    rudder->set_position(PASSED);
+  }
+  else {
+    rudder->set_position(FAILED);
+  }
+  timer->wait(SERVO_WAIT);
 
-
-  // rudder centre, good to go
 }
--- lib/SelfTest/SelfTest.h
+++ lib/SelfTest/SelfTest.h
@@ -1,19 +1,36 @@
 #ifndef SelfTest_h
 #define SelfTest_h
 
+#include "Position.h"
+#include "Gps.h"
+#include "WindSensor.h"
+#include "Compass.h"
+#include "Sail.h"
+#include "Rudder.h"
+#include "Timer.h"
+
+#define NEXT_TEST   22
+#define IN_TEST     45
+#define FAILED      -45
+#define PASSED      0
+#define SERVO_WAIT  30
+
 class SelfTest
 {
   public:
     SelfTest();
-    SelfTest(Gps *gpsp, WindSensor *windsensorp, Compass *compassp, Sail *sailp, Rudder *rudderp);
+    SelfTest(Gps *gpsp, WindSensor *windsensorp, Compass *compassp, Sail *sailp, Rudder *rudderp, Timer *timerp);
     void test();
 
   private:
+    void next();
+    void read_gps(gpsResult *result);
     Gps *gps;
-    Windsensor *windsensor;
+    WindSensor *windsensor;
     Compass *compass;
     Sail *sail;
     Rudder *rudder;
+    Timer *timer;
 };
 
 #endif
