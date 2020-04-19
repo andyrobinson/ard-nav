1e3b0b2707baa25bf4bea01ea253ab70ee5fb628
Andy Robinson <andy.robinson01@bbc.co.uk>
2020-04-19 20:38:16 +0100

    Adding log message

--- lib/Tacker/Tacker.cpp
+++ lib/Tacker/Tacker.cpp
@@ -14,24 +14,24 @@ Tacker::Tacker(Helm *helmp, Compass *compassp, WindSensor *windsensorp, Logger *
 void Tacker::steer(uangle direction, unsigned long steer_time, unsigned long steer_interval) {
 
   angle wind_diff = udiff(windsensor->absolute(compass->bearing()), direction);
+  char logmsg[9]="Straight";
 
   if (abs1(wind_diff) >= TACKER_NO_GO_LIMIT) {
     // no tack
+    logger->msg(logmsg);
     helm->steer(direction, steer_time, steer_interval);
   }
   else {
     short offset = TACKER_NO_GO_LIMIT - abs1(wind_diff);
 
     // first tack
-    char logmsg[6]="Tack1";
-    logger->msg(logmsg);
+    sprintf(logmsg,"Tack1");logger->msg(logmsg);
     uangle tack_direction = uadd(direction, sign(wind_diff) * offset);
     unsigned long tack_time = round(steer_time * cos(to_radians((double) offset)));
     helm->steer(tack_direction, tack_time, steer_interval);
 
     // second tack
-    sprintf(logmsg,"Tack2");
-    logger->msg(logmsg);
+    sprintf(logmsg,"Tack2");logger->msg(logmsg);
     tack_direction = uadd(tack_direction, - sign(wind_diff) * TACKER_NO_GO_LIMIT * 2);
     tack_time = round(steer_time * sin(to_radians((double) offset)));
     helm->steer(tack_direction, tack_time, steer_interval);
