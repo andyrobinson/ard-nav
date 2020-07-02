c08cd8687d9f0fcad90f00ffea440864c5e9797e
Andy Robinson <andy.robinson01@bbc.co.uk>
2020-07-02 21:52:20 +0100

    Updating sail application to use SDLogger

--- sail/sail.ino
+++ sail/sail.ino
@@ -1,6 +1,9 @@
 // Adafruit SSD1306 - Version: Latest
 #include <Wire.h>
 #include <WindSensor.h>
+#include <Compass.h>
+#include <Gps.h>
+#include <SDLogger.h>
 #include <math.h>
 #include <Servo.h>
 #include <Angle.h>
@@ -12,11 +15,20 @@
 WindSensor windsensor;
 Servo sail_servo;
 Sail sail(&sail_servo);
+Compass compass;
+Gps gps;
+SDLogger logger(&gps, &windsensor, &compass);
 
 void setup() {
   windsensor.begin();
+  compass.begin();
+  gps.begin();
+  logger.begin();
+
   sail_servo.attach(SAIL_SERVO_PIN);
 }
+
+char logmsg[22];
 
 void loop() {
   angle wind_angle = 0;
@@ -25,5 +37,7 @@ void loop() {
   wind_angle = windsensor.relative();
   sail.set_position(wind_angle);
 
-  delay(10);
+  sprintf(logmsg, "Sail %4dW,%4dS", wind_angle, sail_servo.read()); logger.msg(logmsg);
+
+  delay(100);
 }
