328f43d4efe1ea8b61dd29b82cd3645f2ae643d9
Andy Robinson <andy.robinson8@gmail.com>
2021-12-19 14:19:44 +0000

    Test5 - remove RotaryPID and Compass

--- lib/Switches/Switches.cpp
+++ lib/Switches/Switches.cpp
@@ -9,14 +9,16 @@ void Switches::begin() {
 }
 
 uint8_t Switches::value() {
-  uint8_t result = (4 * digitalRead(SWITCH4)) + (2 * digitalRead(SWITCH2)) + digitalRead(SWITCH1);
+//  uint8_t result = (4 * digitalRead(SWITCH4)) + (2 * digitalRead(SWITCH2)) + digitalRead(SWITCH1);
+  uint8_t result =0;
   return result;
 }
 
 // 77 to 145
 
 float Switches::dial_percent() {
-  int pin_voltage = analogRead(RCINPUT);
-  float result = 100.0 * ((float) pin_voltage - RCLOWER)/(RCUPPER - RCLOWER);
+  // int pin_voltage = analogRead(RCINPUT);
+  // float result = 100.0 * ((float) pin_voltage - RCLOWER)/(RCUPPER - RCLOWER);
+  float result = 0.0;
   return result;
 }
