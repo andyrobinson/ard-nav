a02fa57a1d882b80dda46dcc28ff28e74ea6f184
Andy Robinson <andy.robinson8@gmail.com>
2022-02-10 20:36:32 +0000

    STABLE: Transplant stable code to Helm

--- lib/Helm/Helm.cpp
+++ lib/Helm/Helm.cpp
@@ -11,59 +11,79 @@ Helm::Helm(Rudder *rudderp, Compass *compassp, Timer *timerp, WindSensor *windse
   rudder_position(0),rudder(rudderp), compass(compassp), timer(timerp), windsensor(windsensorp), sail(sailp), rotarypid(rotarypidp), logger(loggerp), old_heading(0) {}
 
 void Helm::steer(uangle direction, long steer_time, windrange range) {
+
+
     long remaining = steer_time;
 
-    char logmsg[50];
-    sprintf(logmsg, "Steer %4d %8d", direction, steer_time); logger->banner(logmsg);
-
-    angle TEMP_RUDDER = 25;
-    angle TEMP_RELATIVE_WIND = 100;
-    int SAIL_COUNT = 0;
-
-    if (compass->err_percent() >= 10000 && windsensor->err_percent() >= 10000) {
-      sprintf(logmsg, "** I2C Failure **"); logger->banner(logmsg);
-      while (true) {};
-    }
-
-    while ((remaining > 0) && wind_in_range(range)) {
-
-      // exercise I2C a lot
-      long start = millis();
-      while ((millis() - start) < STEER_INTERVAL - 200) {
-        compass->bearing();
-        timer->wait(2);
-        windsensor->relative();
-        timer->wait(4);
+    while (remaining > 0) {
+      for (int i=0; i< 100;i++) {
+        compass.bearing();
+        wind.relative();
+        delay(5);
       }
 
-      angle current_heading;
-      timer->wait(100);
+      Serial.print("Compass: "); Serial.print(compass.bearing());
+      Serial.print(" e: ");Serial.print(compass.err_percent());Serial.print(" | ");
 
-      current_heading = compass->bearing();
-      angle new_rudder_position = rotarypid->calculate(direction, current_heading, STEER_INTERVAL);
+      Serial.print("Wind: "); Serial.print(wind.relative());
+      Serial.print(" e: ");Serial.println(wind.err_percent());
 
-      // set_rudder(new_rudder_position, current_heading);
-     //      sail->set_position(windsensor->relative());
-     set_rudder(TEMP_RUDDER, current_heading);
+      logger.banner(dataString);
 
-     SAIL_COUNT += 1;
-     if (SAIL_COUNT == 10) {
-       SAIL_COUNT = 0;
-       timer->wait(200);
-       sail->set_position(TEMP_RELATIVE_WIND);
-       timer->wait(400);
-       TEMP_RELATIVE_WIND = -TEMP_RELATIVE_WIND;
-     }
-
-     timer->wait(100);
-
-      remaining = remaining - STEER_INTERVAL;
-
-      long turnrate = rot(old_heading, current_heading, STEER_INTERVAL);
-      sprintf(logmsg, "%8d %3d %8d %2d", turnrate, new_rudder_position, remaining, SAIL_COUNT); logger->msg(logmsg);
-
-      TEMP_RUDDER = -TEMP_RUDDER;
+      remaining = remaining - 500;
     }
+
+    // char logmsg[50];
+    // sprintf(logmsg, "Steer %4d %8d", direction, steer_time); logger->banner(logmsg);
+    //
+    // angle TEMP_RUDDER = 25;
+    // angle TEMP_RELATIVE_WIND = 100;
+    // int SAIL_COUNT = 0;
+    //
+    // if (compass->err_percent() >= 10000 && windsensor->err_percent() >= 10000) {
+    //   sprintf(logmsg, "** I2C Failure **"); logger->banner(logmsg);
+    //   while (true) {};
+    // }
+    //
+    // while ((remaining > 0) && wind_in_range(range)) {
+    //
+    //   // exercise I2C a lot
+    //   long start = millis();
+    //   while ((millis() - start) < STEER_INTERVAL - 200) {
+    //     compass->bearing();
+    //     timer->wait(2);
+    //     windsensor->relative();
+    //     timer->wait(4);
+    //   }
+    //
+    //   angle current_heading;
+    //   timer->wait(100);
+    //
+    //   current_heading = compass->bearing();
+    //   angle new_rudder_position = rotarypid->calculate(direction, current_heading, STEER_INTERVAL);
+    //
+    //   // set_rudder(new_rudder_position, current_heading);
+    //  //      sail->set_position(windsensor->relative());
+    //  set_rudder(TEMP_RUDDER, current_heading);
+    //
+    //  SAIL_COUNT += 1;
+    //  if (SAIL_COUNT == 10) {
+    //    SAIL_COUNT = 0;
+    //    timer->wait(200);
+    //    sail->set_position(TEMP_RELATIVE_WIND);
+    //    timer->wait(400);
+    //    TEMP_RELATIVE_WIND = -TEMP_RELATIVE_WIND;
+    //  }
+    //
+    //  timer->wait(100);
+    //
+    //   remaining = remaining - STEER_INTERVAL;
+    //
+    //   long turnrate = rot(old_heading, current_heading, STEER_INTERVAL);
+    //   sprintf(logmsg, "%8d %3d %8d %2d", turnrate, new_rudder_position, remaining, SAIL_COUNT); logger->msg(logmsg);
+    //
+    //   TEMP_RUDDER = -TEMP_RUDDER;
+    // }
 }
 
 void Helm::set_rudder(angle new_position, uangle current_heading) {
