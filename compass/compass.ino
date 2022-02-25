912d7ac6f3012fa4c054380d9339fae939833f67
Andy Robinson <andy.robinson8@gmail.com>
2022-02-25 20:57:38 +0000

    UNSTABLE: moved code into main loop, removed Helm and Switches

--- compass/compass.ino
+++ compass/compass.ino
@@ -17,8 +17,7 @@
 
 #define CHIP_SELECT 4
 
-char dataString[20] = ", new data";
-char logmsg[30];
+char logmsg[50] = "stuff";
 MServo servo_control;
 
 // Simple test for the compass library - should
@@ -31,11 +30,7 @@ WindSensorWire wind;
 Gps gps(&timer);
 SDLogger logger(&gps, &wind, &compass);
 Sail sail(&servo_control);
-RotaryPID rotaryPID(RUDDER_MAX_DISPLACEMENT,&switches,&logger);
 Rudder rudder(&servo_control);
-Helm helm(&rudder, &compass, &timer, &wind, &sail, &rotaryPID, &logger);
-
-char buf[20];
 
 void setup() {
   sercom3.setTimeoutInMicrosWIRE(25000ul, true);  // for new timeout
@@ -50,7 +45,6 @@ void setup() {
   logger.begin();
   sail.begin();
   rudder.begin();
-  switches.begin();
   Serial.println("Starting test");
   sprintf(logmsg, "Starting v%3d.%4d", MAJOR_VERSION, MINOR_VERSION); logger.banner(logmsg);
 
@@ -58,6 +52,36 @@ void setup() {
 
 void loop() {
     Serial.println("Loop");
-    windrange range = {0, 359};
-    helm.steer(90, 30000l, range);
+
+    long remaining = 30000l;
+
+    angle TEMP_RUDDER = 25;
+    angle TEMP_RELATIVE_WIND = 100;
+    int SAIL_COUNT = 0;
+
+    if (compass.err_percent() >= 10000 && wind.err_percent() >= 10000) {
+      Serial.println("I2C failure");
+      sprintf(logmsg, "** I2C Failure **"); logger.banner(logmsg);
+      while (true) {};
+    }
+
+    while (remaining > 0) {
+      // lots of exercise for I2C
+      for (int i=0; i< 100;i++) {
+        compass.bearing();
+        wind.relative();
+        delay(5);
+      }
+
+      angle current_heading = compass.bearing();
+      Serial.print("Steering ..."); Serial.println(current_heading);
+
+      rudder.set_position(TEMP_RUDDER);
+
+      sprintf(logmsg, "%4d", TEMP_RUDDER); logger.msg(logmsg);
+      TEMP_RUDDER = -TEMP_RUDDER;
+
+      remaining = remaining - 800;
+    }
+
 }
