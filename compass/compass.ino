daa271b198c88d56538563d83d3893b8bb5ccc41
Andy Robinson <andy.robinson8@gmail.com>
2022-02-25 21:14:56 +0000

    UNSTABLE: Removed Sail and Serial, may have increased MTBF

--- compass/compass.ino
+++ compass/compass.ino
@@ -5,7 +5,6 @@
 #include <Timer.h>
 #include <Gps.h>
 #include <SDLogger.h>
-#include <Sail.h>
 #include <Rudder.h>
 #include <Helm.h>
 #include <RotaryPID.h>
@@ -14,8 +13,6 @@
 #include <version.h>
 
 #define MAJOR_VERSION 111
-
-#define CHIP_SELECT 4
 
 char logmsg[50] = "stuff";
 MServo servo_control;
@@ -29,38 +26,28 @@ CompassWire compass;
 WindSensorWire wind;
 Gps gps(&timer);
 SDLogger logger(&gps, &wind, &compass);
-Sail sail(&servo_control);
 Rudder rudder(&servo_control);
 
 void setup() {
   sercom3.setTimeoutInMicrosWIRE(25000ul, true);  // for new timeout
   servo_control.begin();
 
-  while (!Serial); // wait for Serial to be ready
-  Serial.begin(19200);
   wind.begin();
   delay(50);
   compass.begin();
   gps.begin();
   logger.begin();
-  sail.begin();
   rudder.begin();
-  Serial.println("Starting test");
   sprintf(logmsg, "Starting v%3d.%4d", MAJOR_VERSION, MINOR_VERSION); logger.banner(logmsg);
 
 }
 
 void loop() {
-    Serial.println("Loop");
-
     long remaining = 30000l;
 
     angle TEMP_RUDDER = 25;
-    angle TEMP_RELATIVE_WIND = 100;
-    int SAIL_COUNT = 0;
 
     if (compass.err_percent() >= 10000 && wind.err_percent() >= 10000) {
-      Serial.println("I2C failure");
       sprintf(logmsg, "** I2C Failure **"); logger.banner(logmsg);
       while (true) {};
     }
@@ -74,7 +61,6 @@ void loop() {
       }
 
       angle current_heading = compass.bearing();
-      Serial.print("Steering ..."); Serial.println(current_heading);
 
       rudder.set_position(TEMP_RUDDER);
 
