d1fec03610635e9806e9569feaad72121603bc44
Andy Robinson <andy.robinson8@gmail.com>
2022-03-27 13:39:10 +0100

    STABLE: Initialise maestro controller in main program then inject

--- lib/MServo/MServo.h
+++ lib/MServo/MServo.h
@@ -4,8 +4,6 @@
 #include "Arduino.h"
 #include "PololuMaestro.h"
 
-#define PIN_SERIAL3_RX       (3ul)
-#define PIN_SERIAL3_TX       (2ul)
 #define SERVO_MIN 2000
 #define SERVO_MAX 10000
 
@@ -14,10 +12,14 @@ class MServo
 {
   public:
     MServo();
+    MServo(MicroMaestro *maestrop);
     void write(uint8_t channel, uint16_t angle);  // unsigned angle
     void setSpeed(uint8_t channel, uint16_t speed);
     void setAccel(uint8_t channel, uint16_t accel);
     void begin();
+
+  private:
+    MicroMaestro *maestro;
 };
 
 #endif
