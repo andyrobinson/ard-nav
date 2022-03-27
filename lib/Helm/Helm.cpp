c86edefe724c18c3866e4cdc9cb6018fb3f47118
Andy Robinson <andy.robinson8@gmail.com>
2022-03-27 19:07:28 +0100

    Removing excess calls

--- lib/Helm/Helm.cpp
+++ lib/Helm/Helm.cpp
@@ -28,7 +28,7 @@ void Helm::steer(uangle direction, long steer_time, windrange range) {
 
     while (remaining > 0) { // && wind_in_range(range)) {
       // lots of exercise for I2C
-      for (int i=0; i< 100;i++) {
+      for (int i=0; i< 1;i++) {
         compass->bearing();
         windsensor->relative();
         delay(5);
