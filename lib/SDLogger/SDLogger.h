50aac7a8942b9fc33c1427cb506c80b49681d291
Andy Robinson <andy.robinson8@gmail.com>
2022-04-08 20:54:01 +0100

    Revert "UNKNOWN: nav.ino with new libs"
    This reverts commit 7e1a0dbcb1e6cd16cbf2d27544450ea72b078efe.

--- lib/SDLogger/SDLogger.h
+++ lib/SDLogger/SDLogger.h
@@ -2,21 +2,19 @@
 #define SDLogger_h
 
 #include "Logger.h"
-#include "WindSensor.h"
 #include "Compass.h"
-#include "Gps.h"
 #include "Utility.h"
 
-#define JAN1_2000_TS 15778l
+#define TEST_LOG_FILE 20001ul
 #define CHIP_SELECT 4
 #define BASE10 10
-#define LOG_INTERVAL 2000ul // may affect battery life
+#define LOG_INTERVAL 300ul // may affect battery life
 #define GPS_WAIT_MILLIS 10
 
 class SDLogger : public Logger {
     public:
       SDLogger();
-      SDLogger(Gps *gpsp, WindSensor *windsensorp, Compass *compassp);
+      SDLogger(Compass *compassp);
       virtual void begin();
       virtual void msg(char *message);
       virtual void banner(char *message);
@@ -28,10 +26,7 @@ class SDLogger : public Logger {
       void calculate_filename(char *filename, long unix_ts);
       boolean sd_time_to_log();
 
-      Gps *gps;
-      WindSensor *windsensor;
       Compass *compass;
-      gpsResult gpsReading;
       char destination;
       char tack;
       char logfile[13];
