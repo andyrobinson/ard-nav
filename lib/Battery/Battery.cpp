5744c2023d68aa0d3e1c8c4a6f2a12ef678cc21f
Andy Robinson <andy.robinson01@bbc.co.uk>
2022-05-09 10:26:40 +0100

    Adding average voltage to Battery

--- lib/Battery/Battery.cpp
+++ lib/Battery/Battery.cpp
@@ -1,9 +1,20 @@
 #include "Battery.h"
 
-Battery::Battery() {}
+Battery::Battery():readings1(0),average1(0.0) {}
 
 float Battery::lipo1v() {
   int pin_voltage = analogRead(LIPO1);
   float result = 3.3 * ((float) pin_voltage)/MAX_ANALOG;
   return result;
 }
+
+float Battery::lipo1avgv() {
+  return average1;
+}
+
+void Battery::add_reading() {
+  float single_value = lipo1v();
+  if (readings1 < SAMPLES) readings1++;
+  average1 = (average1 * (readings1 - 1))/readings1 + single_value/readings1;
+}
+
--- lib/Battery/Battery.h
+++ lib/Battery/Battery.h
@@ -5,12 +5,20 @@
 
 #define LIPO1 A0
 #define MAX_ANALOG 1024.0  // default 10 bits
+#define SAMPLES 10
 
 class Battery
 {
   public:
     Battery();
     float lipo1v();
+    float lipo1avgv();
+
+  private:
+    float average1;
+    int readings1;
+    void add_reading();
+
 };
 
 #endif
