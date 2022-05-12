99c8ddbf5be2c1b6a97cf799e39704b8049ff1ee
Andy Robinson <andy.robinson01@bbc.co.uk>
2022-05-12 09:18:56 +0100

    fixing average battery voltage code (oops)

--- lib/Battery/Battery.cpp
+++ lib/Battery/Battery.cpp
@@ -4,17 +4,18 @@ Battery::Battery():readings1(0),average1(0.0) {}
 
 float Battery::lipo1v() {
   int pin_voltage = analogRead(LIPO1);
-  float result = 3.3 * ((float) pin_voltage)/MAX_ANALOG;
+  float result = 6.6 * ((float) pin_voltage)/MAX_ANALOG;
+  add_reading(result);
   return result;
 }
 
 float Battery::lipo1avgv() {
+  lipo1v();
   return average1;
 }
 
-void Battery::add_reading() {
-  float single_value = lipo1v();
+void Battery::add_reading(float battery_volts) {
   if (readings1 < SAMPLES) readings1++;
-  average1 = (average1 * (readings1 - 1))/readings1 + single_value/readings1;
+  average1 = (average1 * (readings1 - 1))/readings1 + battery_volts/readings1;
 }
 
