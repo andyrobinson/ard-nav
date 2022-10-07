502f4bfb6b8ef901f75b9dc483678200e20f9533
Andy Robinson <andy.robinson8@gmail.com>
2022-10-07 20:31:35 +0100

    Trying to get accelerometer working, WIP

--- compass/compass.ino
+++ compass/compass.ino
@@ -9,6 +9,11 @@
 #define LIS2MDL_OUTX_L_REG  0x68
 #define LIS2MDL_STATUS_REG  0x67
 
+#define COMPASS_ACCEL_I2C_ADDRESS      0x19
+#define ACCEL_REGISTER_OUT_X_L_A       0x28
+#define COMPASS_ACCEL_CTRL_REG1_A      0x20
+
+
 uint8_t write_register_value(uint8_t address, uint8_t reg, uint8_t value) {
   Wire.beginTransmission(address);
   Wire.write((uint8_t) reg);
@@ -32,9 +37,10 @@ void setup() {
   Serial.println("Starting");
 
   // set up at 10Hz continuous, high resolution
-  write_register_value(LIS2MDL_ADDRESS, LIS2MDL_CFG_REG_A, 0x80);  //0x81 for single mode
-
+  write_register_value(LIS2MDL_ADDRESS, LIS2MDL_CFG_REG_A, 0x80);
   write_register_value(LIS2MDL_ADDRESS, LIS2MDL_CFG_REG_C, 0x10);  //block update mode
+
+  write_register_value(COMPASS_ACCEL_I2C_ADDRESS, COMPASS_ACCEL_CTRL_REG1_A, 0x27);
 
 }
 
@@ -42,21 +48,10 @@ void loop() {
 
     Wire.beginTransmission(LIS2MDL_ADDRESS);
 
-    // for single mode
-    // Wire.write(LIS2MDL_CFG_REG_A);
-    // Wire.write(0x81);
-
-    // there is another possible sequence here
-    // write the status reg (0x)
-    // read a byte
-    // repeat until the byte has the Zyxda bit set
-
     Wire.write(LIS2MDL_OUTX_L_REG);
     Wire.requestFrom(LIS2MDL_ADDRESS,6);
-    Serial.print("reading");
-    while (Wire.available() < 6) {
-        Serial.print(".");
-    }
+    while (Wire.available() < 6) { }
+
     byte xlo = Wire.read();
     byte xhi = Wire.read();
     byte ylo = Wire.read();
@@ -65,19 +60,34 @@ void loop() {
     byte zhi = Wire.read();
     Wire.endTransmission();
 
-    Serial.println("");
-    Serial.print("(");Serial.print(xhi); Serial.print(",");Serial.print(xlo);Serial.print(") ");
-    Serial.print("(");Serial.print(yhi); Serial.print(",");Serial.print(ylo);Serial.print(") ");
-    Serial.print("(");Serial.print(zhi); Serial.print(",");Serial.print(zlo);Serial.println(") ");
-
     int x = hilow_toint(xhi,xlo);
     int y = hilow_toint(yhi,ylo);
     int z = hilow_toint(zhi,zlo);
 
-    short bearing = (360 + (short) round(57.2958 * (atan2((float) y, (float) x)))) % 360;
+    Wire.beginTransmission(COMPASS_ACCEL_I2C_ADDRESS);
+    Wire.write(ACCEL_REGISTER_OUT_X_L_A | 0x80);
+    Wire.requestFrom(COMPASS_ACCEL_I2C_ADDRESS,6);
 
-    Serial.print("calc: "); Serial.print(x); Serial.print(","); Serial.print(y); Serial.print(","); Serial.print(z);
-    Serial.print(" [");Serial.print(bearing); Serial.println("]");
+    while (Wire.available() < 6) { }
+
+    xlo = Wire.read();
+    xhi = Wire.read();
+    ylo = Wire.read();
+    yhi = Wire.read();
+    zlo = Wire.read();
+    zhi = Wire.read();
+
+    int xmag = hilow_toint(xhi,xlo);
+    int ymag = hilow_toint(yhi,ylo);
+    int zmag = hilow_toint(zhi,zlo);
+
+    Serial.print(xmag); Serial.print(",");
+    Serial.print(ymag); Serial.print(",");
+    Serial.print(zmag); Serial.print(" | ");
+    // note change in sign because of reversed Y compared to LSM303DLHC
+    short bearing = (360 - (short) round(57.2958 * (atan2((float) y, (float) x)))) % 360;
+
+    Serial.println(bearing);
 
     delay(1000);
 }
--- compass/version.h
+++ compass/version.h
@@ -1,1 +1,1 @@
-#define MINOR_VERSION 116
+#define MINOR_VERSION 119
