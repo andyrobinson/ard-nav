eda10238c61d5f973690498da73336d57fcf469a
andyrobinson <andy.robinson8@gmail.com>
2023-10-08 19:27:11 +0100

    Adjusting SatComm interval, reflashing nav

--- lib/SatComm/SatComm.cpp
+++ lib/SatComm/SatComm.cpp
@@ -6,7 +6,11 @@ SatComm::SatComm(IridiumSBD *modemp, Timer *timerp, Gps *gpsp, Battery *batteryp
 
 // every 15 mins, for test
 const uint8_t SatComm::log_hours[] = {6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
-const uint8_t SatComm::log_minutes[] = {0,10,20,30,40,50};
+const uint8_t SatComm::log_minutes[] = {0,15,30,45};
+
+// every 1 hour, for longer test voyages
+//const uint8_t SatComm::log_hours[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23};
+//const uint8_t SatComm::log_minutes[] = {0};
 
 // every three hours, for live
 // const uint8_t SatComm::log_hours[] = {0,3,6,9,12,15,18,21};
