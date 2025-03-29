1d28092097c4f522cec183f8710d96ebe0955bb0
andyrobinson <andy.robinson8@gmail.com>
2025-03-29 18:44:52 +0000

    Working with SD based config

--- esp32cam/esp32cam.ino
+++ esp32cam/esp32cam.ino
@@ -29,9 +29,10 @@
 #define SKIP_COUNT 15
 #define STABILISATION_WAIT 6000 /* for camera stablisation */
 #define WHITE_LED_GPIO 4
-#define DONE_PIN 13
+#define DONE_PIN 12
 #define EEPROM_SIZE 4
-#define SERIAL_LOGGING 1
+//#define SERIAL_LOGGING 1
+#define DEFAULT_WAKE_SKIP 4
 
 camera_fb_t * fb = NULL;
 fs::FS &filesys = SD_MMC; 
@@ -138,10 +139,8 @@ void initSD() {
   // Turns off the ESP32-CAM white on-board LED (flash) connected to GPIO 4
   // isolate keeps it off in deep sleep
   pinMode(WHITE_LED_GPIO, OUTPUT);
-  digitalWrite(WHITE_LED_GPIO, HIGH);
-  delay(10);
   digitalWrite(WHITE_LED_GPIO, LOW);
-  rtc_gpio_isolate(GPIO_NUM_4);
+  //rtc_gpio_isolate(GPIO_NUM_4);
 
   // Check for an SD card
   uint8_t cardType = SD_MMC.cardType();
@@ -161,6 +160,32 @@ void initSD() {
     #endif
   }
 
+}
+//Read a file in SD card
+int readSkipFromFile(){
+  String path = "/config.txt";
+  int skip = DEFAULT_WAKE_SKIP;
+
+  File file = filesys.open(path.c_str());
+
+  if(file) {
+    Serial.print("Read from file: ");
+    if (file.available()){
+        String line = file.readString();
+        skip = line.toInt();
+        #ifdef SERIAL_LOGGING
+        Serial.print(line);
+        #endif
+    }
+    file.close();
+  }
+  else {
+      #ifdef SERIAL_LOGGING
+      Serial.println("Failed to open file for reading");
+      #endif
+  }
+
+  return skip;
 }
 
 void writeImage(camera_fb_t * image, int pictureNumber) {
@@ -186,10 +211,16 @@ void writeImage(camera_fb_t * image, int pictureNumber) {
 
 void setup() {  
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
-  
+
+  pinMode(DONE_PIN, OUTPUT); // trying this really early because of issues with pin being high
+  digitalWrite(DONE_PIN, LOW); // may need to put after SD init
+
   #ifdef SERIAL_LOGGING
   Serial.begin(115200);
   #endif
+
+  initSD(); // because this defines IO pins and is needed for wake skip
+  int wakeskip = readSkipFromFile();
 
   // read and increment pictureNumber
 
@@ -200,43 +231,53 @@ void setup() {
   EEPROM.put(0, pictureNumber);
   EEPROM.commit();
 
-  initCamera();
-  initSD();
+  if (pictureNumber % wakeskip == 0) {
+    initCamera();
+    stabliseCamera();
 
-  stabliseCamera();
+    #ifdef SERIAL_LOGGING
+    Serial.println("Click!");
+    #endif
 
-  #ifdef SERIAL_LOGGING
-  Serial.println("Click!");
-  #endif
+    fb = esp_camera_fb_get();  
 
-  fb = esp_camera_fb_get();  
-
-  if(fb) {
-    writeImage(fb, pictureNumber);
-    esp_camera_fb_return(fb);
-  } 
+    if(fb) {
+      writeImage(fb, pictureNumber);
+      esp_camera_fb_return(fb);
+    } 
+    #ifdef SERIAL_LOGGING
+    else {
+      Serial.println("Camera capture failed");
+    }
+    #endif
+  }
   #ifdef SERIAL_LOGGING
   else {
-    Serial.println("Camera capture failed");
+    Serial.println("Skipping this time");
   }
-
   Serial.println("Going to sleep now");
   Serial.flush(); 
   #endif
 
-  pinMode(DONE_PIN, OUTPUT);
 
 }
 
 void loop() {
+  bool sent = false;
   while (1) {
+    digitalWrite(WHITE_LED_GPIO, HIGH);
+
   #ifdef SERIAL_LOGGING
-  Serial.println("Signalling sleep");
+  if (!sent) {
+    Serial.println("Signalling sleep");
+    sent = true;
+  }
   #endif
     digitalWrite(DONE_PIN, HIGH);
-    delay(1);
+    delay(10);
     digitalWrite(DONE_PIN, LOW);
-    delay(1);
+    digitalWrite(WHITE_LED_GPIO, LOW);
+    delay(50);
   }
 
 }
